2e73b251d7c2ec6db9f0467b571a04141f81b4b2
scarletjara <scarlet.jara@hotmail.com>
2025-12-05 04:28:22 -0300

    api devuelve mas datos del usuario

--- src/main/java/cl/detoxnow/Usuarios/DTO/LoginResponse.java
+++ src/main/java/cl/detoxnow/Usuarios/DTO/LoginResponse.java
@@ -1,13 +1,17 @@
 package cl.detoxnow.usuarios.dto;
 
+import cl.detoxnow.Usuarios.Model.Usuario;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
 
     private boolean success;
     private String mensaje;
+    private Usuario usuario;
     
 }
