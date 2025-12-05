cf7eb97fcfec3461903b2ef8a4645eca2c3cb7e0
scarletjara <scarlet.jara@hotmail.com>
2025-12-05 04:33:39 -0300

    se vuelve a version anterior

--- src/main/java/cl/detoxnow/Usuarios/DTO/LoginResponse.java
+++ src/main/java/cl/detoxnow/Usuarios/DTO/LoginResponse.java
@@ -1,16 +1,13 @@
 package cl.detoxnow.usuarios.dto;
 
-import cl.detoxnow.usuarios.model.Usuario;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class LoginResponse {
 
     private boolean success;
     private String mensaje;
-    private Usuario usuario;
+    
 }
